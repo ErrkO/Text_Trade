141613dc39aa86bf2849721adf658977eac54f4c
malaknight <eoliver4@my.apsu.edu>
2017-04-10 12:06:12 -0500

    eddited the moderator class

--- Text_Trade/UserList.cs
+++ Text_Trade/UserList.cs
@@ -4,6 +4,9 @@
 //     Changes to this file will be lost if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+
+// Eric TODO
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
