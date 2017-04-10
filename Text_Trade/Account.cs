7d258c525b4ae207f64b3f4723808c6a9f5c037c
Linh Do <ldo@my.apsu.edu>
2017-04-10 11:40:56 -0500

    resolve conflict

--- Text_Trade/Account.cs
+++ Text_Trade/Account.cs
@@ -82,7 +82,7 @@ public class Account
         email = eMail;
 	}
 
-	public void ChangePassword(string pass)  //why does this method return something? make it void
+	public void ChangePassword(string pass)  
 	{
         password = pass;
 	}
