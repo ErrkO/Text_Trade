48d06f9737571c4ca23199a7392027cf8638d440
Linh Do <ldo@my.apsu.edu>
2017-04-13 14:12:49 -0500

    edit form

--- Text_Trade/CreateListing.Designer.cs
+++ Text_Trade/CreateListing.Designer.cs
@@ -40,7 +40,6 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
@@ -48,6 +47,7 @@
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -87,7 +87,7 @@
             // 
             this.textBox3.Location = new System.Drawing.Point(89, 125);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(84, 20);
+            this.textBox3.Size = new System.Drawing.Size(91, 20);
             this.textBox3.TabIndex = 4;
             // 
             // label3
@@ -101,15 +101,15 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(250, 125);
+            this.textBox4.Location = new System.Drawing.Point(246, 125);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(113, 20);
+            this.textBox4.Size = new System.Drawing.Size(117, 20);
             this.textBox4.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(192, 128);
+            this.label4.Location = new System.Drawing.Point(205, 128);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 7;
@@ -121,7 +121,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(89, 163);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(84, 21);
+            this.comboBox1.Size = new System.Drawing.Size(91, 21);
             this.comboBox1.TabIndex = 8;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -149,21 +149,13 @@
             // 
             this.textBox5.Location = new System.Drawing.Point(89, 203);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(84, 20);
+            this.textBox5.Size = new System.Drawing.Size(91, 20);
             this.textBox5.TabIndex = 11;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(265, 163);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(98, 21);
-            this.comboBox2.TabIndex = 12;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(205, 166);
+            this.label7.Location = new System.Drawing.Point(212, 166);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(54, 13);
             this.label7.TabIndex = 13;
@@ -172,15 +164,15 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(265, 203);
+            this.textBox6.Location = new System.Drawing.Point(272, 203);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(98, 20);
+            this.textBox6.Size = new System.Drawing.Size(91, 20);
             this.textBox6.TabIndex = 14;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(225, 206);
+            this.label8.Location = new System.Drawing.Point(232, 206);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(34, 13);
             this.label8.TabIndex = 15;
@@ -225,11 +217,20 @@
             this.label10.TabIndex = 19;
             this.label10.Text = "Enter all fields below to create a listing";
             // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(272, 163);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(91, 21);
+            this.comboBox3.TabIndex = 20;
+            // 
             // CreateListing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(404, 389);
+            this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox7);
@@ -237,7 +238,6 @@
             this.Controls.Add(this.label8);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -271,7 +271,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label8;
@@ -279,5 +278,6 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
