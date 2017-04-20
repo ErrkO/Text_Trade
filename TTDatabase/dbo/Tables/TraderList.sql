081c313f9039032aadd6483e39bf8e00732e7043
malaknight <eoliver4@my.apsu.edu>
2017-04-19 23:35:39 -0500

    moderator should work for the most part. Most of the database should be usable now

--- TTDatabase/dbo/Tables/TraderList.sql
+++ TTDatabase/dbo/Tables/TraderList.sql
@@ -3,6 +3,7 @@
 	[trader_id] INT NOT NULL PRIMARY KEY, 
     [username] VARCHAR(100) NOT NULL, 
     [password] VARCHAR(50) NOT NULL, 
-    [deleted] BINARY(1) NOT NULL, 
-    [classSchedule] NCHAR(10) NULL
+    [deleted] BINARY(1) NOT NULL DEFAULT 0, 
+    [classSchedule] NCHAR(10) NULL, 
+    [warnings] INT NOT NULL DEFAULT 0
 )
