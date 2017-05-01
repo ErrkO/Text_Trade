80642b57f6faf6012118ff8fb7d4f41ca73fb893
Linh Do <ldo@my.apsu.edu>
2017-05-01 00:05:50 -0500

    Add SearchbyBooktitle() method in Marketplace.cs Change Login window a bit, for Password field, use PasswordBox instead

--- Text_Trade/Forms/Coming_soon.xaml.cs
+++ Text_Trade/Forms/Coming_soon.xaml.cs
@@ -23,5 +23,10 @@ namespace Text_Trade.Forms
         {
             InitializeComponent();
         }
+
+        private void button_OK_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
